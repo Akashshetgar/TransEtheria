fda5c2a99fe1d05313ad5285d866b7d9fd4df210
Dwayne232001 <67543073+Dwayne232001@users.noreply.github.com>
2022-04-09 17:45:19 +0530

    First Front-end

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <p className="text-center text-green-500 font-bold">
+        This is the homepage
+    </p>
+  )
+}
+
+export default Home;
---
+++ src/components/Registration.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Registration = () => {
+  return (
+    <form>
+      <label for="name">Name: </label>
+      <input type="text" id='name'></input><br></br>
+      <label for="name">Address: </label>
+      <input type="text" id='name'></input><br></br>
+      <label for="name">Password: </label>
+      <input type="text" id='name'></input><br></br>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={(e) => {
+      e.preventDefault();
+      window.location.href='http://google.com';
+      }}>
+        Button
+      </button>
+    </form>
+  )
+}
+
+export default Registration;
