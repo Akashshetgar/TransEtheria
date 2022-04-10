4b8edb7caef9f5c80f5b4748f80983c6e7ca2784
aayush18602 <83156674+aayush18602@users.noreply.github.com>
2022-04-10 07:11:03 +0530

    Almost Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,12 +57,12 @@ function App() {
       flex-col p-3 rounded-md hover:shadow-2xl">
         <div className="flex flex-col items-center w-full and mt-3">
           <div className='="w-full mb-6 p-2'>
-            <a href={'https://ropsten.etherscan.io/address/${addressFrom}'} target="_blank" rel="noopener noreferrer">
+            <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener noreferrer">
               <p className="text-white text-base">
                 From: {shortenAddress(addressFrom)}
               </p>
             </a>
-            <a href={'https://ropsten.etherscan.io/address/${addressTo}'} target="_blank" rel="noopener noreferrer">
+            <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noopener noreferrer">
               <p className="text-white text-base">
                 To: {shortenAddress(addressTo)}
               </p>
